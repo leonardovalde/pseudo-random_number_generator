1d9146d1f381878b7f5feb996db43255ba79d43e
Leonardo Valderrama <leonardovalde16@gmail.com>
2023-03-26 20:47:41 -0500

    add mean square doc

--- CuadradosMedios.js
+++ CuadradosMedios.js
@@ -1,9 +1,11 @@
+//Definimos el boton de calcualr y le asociamos el metodo principal
 let miBoton = document.getElementById("miBoton");
 miBoton.addEventListener("click", runMethod);
 
 let Ri = [];
 let Ni = [];
 
+//Este metodo, crea la tabla vacia en el html, y obitiene las entradas del metodo
 function runMethod() {
   let tablas = document.getElementById("tablas");
   tablas.innerHTML =
@@ -15,29 +17,31 @@ function runMethod() {
   const tabla = document
     .getElementById("tablaOne")
     .getElementsByTagName("tbody")[0];
-  tabla.innerHTML = "";
-  Ri = [];
+  tabla.innerHTML = "";//Limpia la tabla si tenia una ejecucion anterior
+  Ri = [];//Limpia el array de Ri si tenia una ejecucion anterior
   let iterationsList = [];
   for (let i = 0; i < iterations; i++) {
-    iterationsList.push(makeIteration(Xi));
-    tabla.appendChild(pushInTable(i, iterationsList));
+    iterationsList.push(makeIteration(Xi));//Guarda en en el array de iteraciones el array de la fila correspondinte al Ri
+    tabla.appendChild(pushInTable(i, iterationsList));//Coloca la fila en la tabla del html
     Ri.push(iterationsList[iterationsList.length - 1][4]);
     Xi = iterationsList[iterationsList.length - 1][3];
   }
   if (minimo != 0 && maximo != 0) {
-    Ni = calculateNi(Ri, minimo, maximo);
-    makeNiTable(Ni);
+    Ni = calculateNi(Ri, minimo, maximo);//LLena el array de Ni
+    makeNiTable();//Crea la tabla en el html de el Ni
   }
 }
 
+//Calcula el Ni, tiene como entrada el maximo, el minimo y Ri
 function calculateNi(Ri, min, max) {
   Ni = [];
   Ri.forEach((element) => {
-    Ni.push(min + (max - min) * element);
+    Ni.push(min + (max - min) * element);//Lo guarda en el array de Ni
   });
   return Ni;
 }
 
+//Este metodo, tiene como entrada el Xi, y con este se encarga de realizar la fila correspondiente
 function makeIteration(Xi) {
   let square = Xi ** 2;
   let extension = String(square).length;
@@ -62,6 +66,24 @@ function makeIteration(Xi) {
   return [Xi, square, extension, +extaction, +(extaction * 0.0001).toFixed(4)];
 }
 
+//Construye la tabla de Ni en el html
+function makeNiTable() {
+  let tablas = document.getElementById("tablas");
+  tablas.innerHTML +=
+    '<table id="tablaTwo"><thead><tr><th>Ni</th></tr></thead><tbody></tbody></table>';
+  let tabla = document
+    .getElementById("tablaTwo")
+    .getElementsByTagName("tbody")[0];
+  Ni.forEach((element) => {
+    const fila = document.createElement("tr");
+    const niColumn = document.createElement("td");
+    niColumn.textContent = element.toFixed(4);
+    fila.appendChild(niColumn);
+    tabla.appendChild(fila);
+  });
+}
+
+//Este metodo se encarga de crear la tabla que cotnien la iteracion, el Xi, el Xi al cuadrado, la extencion, la extraccion y el Ri
 function pushInTable(i, iterationsList) {
   const fila = document.createElement("tr");
 
@@ -92,45 +114,35 @@ function pushInTable(i, iterationsList) {
   return fila;
 }
 
-function makeNiTable(Ni) {
-  let tablas = document.getElementById("tablas");
-  tablas.innerHTML +=
-    '<table id="tablaTwo"><thead><tr><th>Ni</th></tr></thead><tbody></tbody></table>';
-  let tabla = document
-    .getElementById("tablaTwo")
-    .getElementsByTagName("tbody")[0];
-  Ni.forEach((element) => {
-    const fila = document.createElement("tr");
-    const niColumn = document.createElement("td");
-    niColumn.textContent = element.toFixed(4);
-    fila.appendChild(niColumn);
-    tabla.appendChild(fila);
-  });
-}
 
 
 
-let modal = document.getElementById("myModal");
-let span = document.getElementsByClassName("close")[0];
+//En esta parte se definen los elementos de la grafica
+let modal = document.getElementById("myModal");//Contenedor de la grafica
+let span = document.getElementsByClassName("close")[0];//Boton de cerrar
 
+//Se define el boton de graficar Ri y el metodo para la grafica
 let graficar_Ri = document.getElementById("Graficar_Ri");
 graficar_Ri.onclick = function() {
   let ranges = Number.parseInt(document.getElementById("iterations").value);
-  makeIntervalsTable(ranges/5, Ri);
-  modal.style.display = "block";
+  makeIntervalsTable(ranges/5, Ri);//Este metodo se encarga de crear la tabla de frecuencia en los intervalos
+  modal.style.display = "block";//Aqui se muestra la grafica
 }
 
+//Se define el boton de graficar Ni y el metodo para la grafica
 let graficar_Ni = document.getElementById("Graficar_Ni");
 graficar_Ni.onclick = function() {
   let ranges = Number.parseInt(document.getElementById("iterations").value);
-  makeIntervalsTable(ranges/5, Ni);
-  modal.style.display = "block";
+  makeIntervalsTable(ranges/5, Ni);//Este metodo se encarga de crear la tabla de frecuencia en los intervalos
+  modal.style.display = "block";//Aqui se muestra la grafica
 }
 
+//Este metodo el boton de cerrar oculta la grafica
 span.onclick = function() {
   modal.style.display = "none";
 }
 
+//Este metodo el boton de cerrar oculta la grafica cuando se clickea fuera de ella
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
